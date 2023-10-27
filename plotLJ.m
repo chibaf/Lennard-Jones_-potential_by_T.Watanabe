5d6d66cc736e68d39acbd2226ffbb287af3de86e
chibaf <chibaf@mac.com>
2023-10-27 11:22:01 +0900

    Add files via upload

--- plotLJ.m
+++ plotLJ.m
@@ -1,5 +1,8 @@
 function plotLJ(a)
   b=a';
-  plot(b(1,1:end),b(2,1:end),"-;KE;",b(1,1:end),b(3,1:end),"-;PE;",b(1,1:end),b(4,1:end),"-;KE+KP;",b(1,1:end),b(5,1:end),"-;T;");
+  plot(b(1,1:end),b(2,1:end),"-;KE;",
+  b(1,1:end),b(3,1:end),"-;PE;",
+  b(1,1:end),b(4,1:end),"-;KE+KP;",
+  b(1,1:end),b(5,1:end),"-;T;");
   xlabel ("t");
 endfunction
